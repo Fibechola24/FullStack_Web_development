0ab5f51c54503daf25a96df73edc15d7edcc506f
Victor Okpanachi <okpanachivictor0@gmail.com>
2023-02-06 22:30:26 +0100

    DOM

---
+++ JavaScript/20 - The DOM/the-dom.js
@@ -0,0 +1,27 @@
+// function init(){
+//     const p = document.querySelector('p');
+//     console.log(p);
+// }
+// init();
+
+// // const item2 = document.querySelector('.item');
+// console.log(item2);
+// const divs = document.querySelector(divs);
+// console.log(divs);
+// const item2 = document.querySelector('.item2');
+// const item2image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
+// console.log(heading);
+// console.dir(heading.textContent);  //getter
+// heading.textContent = 'I am happy';
+// console.log(heading.textContent);
+
+// console.dir(heading.textContent);
+// console.log(heading.innerText);
+
+ 
+
+
+
+
+
